2a13f09512814ff44d708a70436705a62b9ac8eb
Lucas <luc.coulson6@gmail.com>
2025-07-03 11:06:17 -0400

    started making game logic

--- script.js
+++ script.js
@@ -18,16 +18,18 @@
 // grid gameboard turned into an object
 function Gameboard(){
     return {
-        row1: [1, 2, 3],
-        row2: [4, 5, 6],
-        row3: [7, 8, 9],
+        row1: [1, 1, 1],
+        row2: [2, 2, 2],
+        row3: [3, 3, 3],
+
         printGameboard(){
             for (let key in this) {
                 if (Array.isArray(this[key])) {
                     console.log(this[key].join(' '));
                 }
             }
-        }
+        },
+
     };
 
 }
@@ -35,16 +37,50 @@ function Gameboard(){
 const gameboard = Gameboard();
 gameboard.printGameboard();
 
+function gameController(
+    playerOneName = "player one",
+    playerTwoName = "player two"
+) {
+    const board = Gameboard();
+    
+    // two players playing the gamed
+    const players = [{
+        name: playerOneName,
+        symbol: "X"
+    },
+    {
+        name: playerTwoName,
+        symbol: "O"
+    },
+];
+
+    const playerTurn = players[0];
+    
+    // that player is gonna make there move
+
+
+
+
+}
+
 
 // player object
 function player(name, symbol) {
     return{
         name,
         symbol,
-    };
+
+        }
 };
 
-let player1 = player("player1", "X");
-let player2 = player("Player2", "O");
+const player1 = player("player1", "X");
+const player2 = player("Player2", "O");
 
+
+
+//user input 
+//create a user input function
+// create another function that will call those with the player objects
+//prompt 1 and 2 go in as the array values
+//if index not an X or O replace with player symbol
 
